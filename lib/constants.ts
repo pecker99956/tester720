f6b9195c9192a1d35f64a5a762277ad84800a975
pecker99956 <86660487+pecker99956@users.noreply.github.com>
2024-09-29 21:04:27 +0530

    Add files via upload

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const STREAMWISH_BASE_URL =
+    process.env.STREAMWISH_BASE_URL || "https://api.streamwish.com";
+export const STREAMWISH_API_KEY = process.env.STREAMWISH_API_KEY;
+export const SITENAME = process.env.SITENAME || "Streamwish";
+export const DEFAULT_PER_PAGE = parseInt(process.env.DEFAULT_PER_PAGE || "40");
+export const DEFAULT_REVALIDATE_INTERVAL = 3600;
